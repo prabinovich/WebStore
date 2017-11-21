902e25337d3a4e95254776c460e10e678800ec4b
prabinovich <p_rabinovich@yahoo.com>
2017-11-21 10:39:45 -0500

    Added database scripts

---
+++ db/webstore_data.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM webstore.products
+LIMIT 0, 1000
+
+-- Date: 2017-11-21 10:20
+*/
+INSERT INTO `products` (`productid`,`name`,`imagefile`,`price`) VALUES (1,'Arabic t-shirt','ArabicTShirt.jpg',10);
+INSERT INTO `products` (`productid`,`name`,`imagefile`,`price`) VALUES (2,'Chinese t-shirt','ChineseTShirt.jpg',17);
+INSERT INTO `products` (`productid`,`name`,`imagefile`,`price`) VALUES (3,'Hebrew t-shirt','HebrewTShirt.jpg',19);
+INSERT INTO `products` (`productid`,`name`,`imagefile`,`price`) VALUES (4,'Hoodie','Hoodie.jpg',50);
+INSERT INTO `products` (`productid`,`name`,`imagefile`,`price`) VALUES (5,'Keychain','Keychain.jpg',3);
+INSERT INTO `products` (`productid`,`name`,`imagefile`,`price`) VALUES (6,'Lanyard','Lanyard.jpg',2);
+INSERT INTO `products` (`productid`,`name`,`imagefile`,`price`) VALUES (7,'Thermos','Thermos.jpg',35);
---
+++ db/webstore_ddls.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE `webstore` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE TABLE `products` (
+  `productid` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `imagefile` varchar(100) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  PRIMARY KEY (`productid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
