c079f5c14e868dc5bd4984f5c57699303dce5507
prabinovich <p_rabinovich@yahoo.com>
2017-11-28 10:22:28 -0500

    Moved user check to DBconnection class

--- src/store/DBConnection.java
+++ src/store/DBConnection.java
@@ -10,8 +10,8 @@ import org.apache.log4j.Logger;
 
 public class DBConnection {
 	
-	private static String account = "appuser"; 
-	private static String password = "Password1234%"; 
+	private static String dbaccount = "appuser"; 
+	private static String dbpassword = "Password1234%"; 
 	private static String server = "localhost:3306";
 	private static String database = "webstore"; 
 	
@@ -19,11 +19,39 @@ public class DBConnection {
 		
 	}
 	
+	public ResultSet checkUserLogin(String uname, String upass){
+		ResultSet rs = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection( "jdbc:mysql://" + server, dbaccount ,dbpassword);
+			Statement stmt = con.createStatement();
+			stmt.executeQuery("USE " + database);
+	        String sql = "SELECT * FROM users WHERE username='" + uname + "' AND password='" + upass + "'";
+	        rs = stmt.executeQuery(sql);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			
+			Logger logger = Logger.getLogger(this.getClass());
+			BasicConfigurator.configure();
+			logger.error(e.toString());
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			
+			Logger logger = Logger.getLogger(this.getClass());
+			BasicConfigurator.configure();
+			logger.error(e.toString());
+		}
+		
+		return rs;
+	}
+	
 	public ResultSet queryDBWith(String query) {
 		ResultSet rs = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection( "jdbc:mysql://" + server, account ,password);
+			Connection con = DriverManager.getConnection( "jdbc:mysql://" + server, dbaccount ,dbpassword);
 			Statement stmt = con.createStatement();
 			stmt.executeQuery("USE " + database);
 			if(query.equals("")) {
--- src/store/login.java
+++ src/store/login.java
@@ -2,11 +2,8 @@ package store;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
 
-import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,11 +17,6 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name = "login", urlPatterns = {"/login"})
 public class login extends HttpServlet {
 
-	private static String dbaccount = "appuser"; 
-	private static String dbpassword = "Password1234%"; 
-	private static String server = "localhost:3306";
-	private static String database = "webstore"; 
-	
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -34,14 +26,8 @@ public class login extends HttpServlet {
             String password = request.getParameter("password");
 
             try {
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection conn = DriverManager.getConnection("jdbc:mysql://" + server, dbaccount ,dbpassword);
-
-                Statement st = conn.createStatement();
-                st.executeQuery("USE " + database);
-                String sql = "SELECT * FROM users WHERE username='" + username + "' AND password='" + password + "'";
-                System.out.println(sql);
-                ResultSet rs = st.executeQuery(sql);
+            	DBConnection db = new DBConnection();
+        		ResultSet rs = db.checkUserLogin(username, password);
 
                 if (rs.next()) {
                     HttpSession session = request.getSession();
@@ -52,7 +38,6 @@ public class login extends HttpServlet {
                     out.println("<p>");
                     out.println("<a href=\"login.jsp\">Login again</a>");
                 }
-                conn.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
