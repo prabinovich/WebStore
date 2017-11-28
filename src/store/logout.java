daf3c4141d4d6815948ab82eac9a7c416e6aeac8
prabinovich <p_rabinovich@yahoo.com>
2017-11-27 23:23:35 -0500

    removed some imports

--- src/store/logout.java
+++ src/store/logout.java
@@ -1,8 +1,6 @@
 package store;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
